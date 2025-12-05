f5507a8fe9e53468ca39d812906b8405fe05cec7
Clive Caseley <clive@caseley.com>
2025-12-05 17:01:56 +0000

    Revert "Batch 3: Extract Rightmove URLs and target asking price (#11)"
    This reverts commit b5d2797046479d6f28be91df26bfee8ca50c2a5a.

--- src/utils/excelHelper.js
+++ src/utils/excelHelper.js
@@ -21,10 +21,6 @@ function generateHyperlink(url, text = 'View') {
  * - EPC Certificate → EPC Certificate Link (new)
  * - Google Streetview URL → Google Streetview Link (already exists in geocoder)
  * 
- * BATCH 3: Add separate URL columns for Rightmove and PropertyData
- * - URL_Rightmove → URL_Rightmove (hyperlink)
- * - URL_PropertyData → URL_PropertyData (hyperlink)
- * 
  * @param {Array<Object>} properties - Array of properties
  * @returns {Array<Object>} Properties with Link columns
  */
@@ -42,24 +38,6 @@ function addHyperlinks(properties) {
             property['EPC Certificate Link'] = generateHyperlink(property['EPC Certificate'], 'View EPC');
         } else {
             property['EPC Certificate Link'] = '';
-        }
-        
-        // BATCH 3: Rightmove URL hyperlink
-        if (property.URL_Rightmove) {
-            // Store both the raw URL and hyperlink formula
-            // Excel will display the hyperlink formula as a clickable link
-            property['Rightmove Link'] = generateHyperlink(property.URL_Rightmove, 'View RM');
-        } else {
-            property['Rightmove Link'] = '';
-        }
-        
-        // BATCH 3: PropertyData URL hyperlink
-        if (property.URL_PropertyData) {
-            // Store both the raw URL and hyperlink formula
-            // Excel will display the hyperlink formula as a clickable link
-            property['PropertyData Link'] = generateHyperlink(property.URL_PropertyData, 'View PD');
-        } else {
-            property['PropertyData Link'] = '';
         }
         
         // Note: Google Streetview Link is already added in geocoder.js
